5a524296eabbae2886e4c0505f292dce05399cd3
Emile Joseph <emmden17@yahoo.com>
2020-02-15 13:50:36 -0600

    initial commit

---
+++ logic.js
@@ -0,0 +1,44 @@
+var myMap = L.map("map", {
+  center: [15.5994, -28.6731],
+  zoom: 3
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets-basic",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Country data
+var countries = [];
+
+
+
+// Loop through the cities array and create one marker for each city object
+for (var i = 0; i < countries.length; i++) {
+
+  // Conditionals for countries points
+  var color = "";
+  if (countries[i].points > 200) {
+    color = "yellow";
+  }
+  else if (countries[i].points > 100) {
+    color = "blue";
+  }
+  else if (countries[i].points > 90) {
+    color = "green";
+  }
+  else {
+    color = "red";
+  }
+
+  // Add circles to map
+  L.circle(countries[i].location, {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: color,
+    // Adjust radius
+    radius: countries[i].points * 1500
+  }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+}
